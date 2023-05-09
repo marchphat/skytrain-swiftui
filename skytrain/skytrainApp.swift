9dfcb1b72bdd77b0a5cb136ebb2b066316df95d9
Nantanat Thongthep <phat.nantanat@gmail.com>
2023-05-09 13:25:52 +0700

    Initial Commit

---
+++ skytrain/skytrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  skytrainApp.swift
+//  skytrain
+//
+//  Created by Nantanat Thongthep on 9/5/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct skytrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
